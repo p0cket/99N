6e353d7a81cef3a125620214cf9907ab07991bcf
Pocket <p0ck31@gmail.com>
2020-09-14 22:26:28 -0400

    Added attack animation, and next is enemy damaged

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.E)){
-            Debug.Log('E');
             Attack();
         }
     }
 
     void Attack() {
         // Play attack animation
+        animator.SetTrigger("Attacking");
+
         // detect whats in range
-        // do
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(
+            attackPoint.position, attackRange, enemyLayers
+        );
+
+        // damage them
+        foreach(Collider2D enemy in hitEnemies) {
+            Debug.Log("We hit" + enemy.name);
+        }
+    }
+
+    void OnDrawGizmosSelected(){
+        if (attackPoint == null) {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
+// https://youtu.be/sPiVz1k-fEs?t=786 melee combat tutorial

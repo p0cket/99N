8786a02e6174aedc61d29e876caefcd4955f41b5
Pocket <p0ck31@gmail.com>
2020-09-16 13:13:40 -0400

    basic explosions when press R

---
+++ Assets/endExplosion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endExplosion : MonoBehaviour
+{
+    public float timer = 0.0f;
+    public float endTime;
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= endTime) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/explode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explode : MonoBehaviour {
+
+    public GameObject explosion;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)){
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            gameObject.SetActive(false);
+        }
+    }
+}
